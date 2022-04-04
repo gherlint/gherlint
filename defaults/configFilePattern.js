eaaf08e073999da47efe2ea9a43d6b5e012447f0
Saw-jan <saw.jan.grg3e@gmail.com>
2022-04-04 08:38:02 +0545

    add default configs

---
+++ defaults/configFilePattern.js
@@ -0,0 +1,7 @@
+/**
+ * Allowed config files names:
+ * .gherlintrc
+ * .gherlintrc.js
+ * .gherlintrc.json
+ */
+module.exports = ".gherlintrc?(.js?(on))";
---
+++ defaults/gherlintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    formatRules: {
+        indentStyle: "space",
+        indentSize: 4,
+        newLine: false,
+    },
+    rules: {
+        indentation: "error",
+    },
+    ignorePatterns: [],
+};
