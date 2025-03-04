7a67989ecc690f226c44a879809c906eddc88206
ishabaral <baralisha.16@gmail.com>
2025-03-04 12:21:20 +0545

    fix: es2021 error and update eslint and related files

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
+
+export default [
+    js.configs.recommended,
+    {
+        ignores: ["node_modules/*"], 
+    },
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                console: "readonly",
+                require: "readonly",
+                module: "readonly",
+                __dirname: "readonly",
+                process: "readonly",
+                global: "readonly",
+
+                // Jest globals
+                afterAll: "readonly",
+                afterEach: "readonly",
+                beforeAll: "readonly",
+                beforeEach: "readonly",
+                describe: "readonly",
+                expect: "readonly",
+                it: "readonly",
+                jest: "readonly",
+                test: "readonly",
+            },
+        },
+        plugins: {
+            jest,
+        },
+        rules: {
+            indent: ["error", 4, { SwitchCase: 1 }],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
+    },
+];
--- utils/packageInfo.js
+++ utils/packageInfo.js
@@ -1,7 +1,7 @@
 const { resolve, join } = require("path");
-const package = require(join(resolve(__dirname), "..", "package.json"));
+const packageInfo = require(join(resolve(__dirname), "..", "package.json"));
 
 module.exports = {
-    getModuleName: () => package.name,
-    getModuleVersion: () => package.version,
+    getModuleName: () => packageInfo.name,
+    getModuleVersion: () => packageInfo.version,
 };
